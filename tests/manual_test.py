d4f4d19db6ca25ae8fb08371ab6d7cbc34c541fc
Anton <109826223+Arcadiyyyyyyyy@users.noreply.github.com>
2024-01-12 15:45:47 +0000

    ci: change bump commit name

--- tests/manual_test.py
+++ tests/manual_test.py
@@ -1,3 +1,4 @@
+from typing import Awaitable
 from tradelink import Portfolio
 import asyncio
 import logging
@@ -7,8 +8,12 @@ from datetime import datetime
 async def main():
     logging.basicConfig(level=10)
     start_time = datetime.utcnow()
-    x = Portfolio("f7ac1da1-59a5-43e4-a629-88e65a411e17", start_date=datetime(2023, 12, 1))
-    print(await x.get_start_date())
+    portfolios: list[Awaitable[datetime]] = []
+    for _ in range(100):
+        portfolios.append(Portfolio("f7ac1da1-59a5-43e4-a629-88e65a411e17", start_date=datetime(2023, 12, 1)).get_end_date())
+    
+    x = await asyncio.gather(*portfolios)
+    print(x)
     end_time = datetime.utcnow()
     print(end_time - start_time)
 
